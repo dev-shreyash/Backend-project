2e6df12b0396e05810e7f67760a3c511afb65cf9
Shreyash-69 <bhosaleshreyash2@gmail.com>
2024-04-02 15:20:54 +0530

    Added cookie-parser and cors middleware to the project

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
 import dotenv from  'dotenv';
-import express from "express"
 import connectDB from "./db/index.js";
 
 dotenv.config({
     path:'./.env'
 })
-//const app =express()
+
 
 connectDB()
-// (
-//     async () => {
-//         try{
-//             await Mongoose.connect(`${process.env.MANGODB_URL}/${DB_NAME}`)
-//             app.on("error",()=>{
-//                 console.log('MongoDB connection error:', err);
-//                 throw err
-//             })
-//             app.listen(process.env.PORT,()=>{
-//                 console.log(`Server is running on port ${process.env.PORT}`);
-//             })
-//         }
-//         catch (err) {
-//             console.log('MongoDB connection error:', err);
-//             throw err
-//         }
-//     })()
+.then(()=>{
+    a00.on("error",(err)=>{
+        console.log("error",err)
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running on ${process.env.PORT}`)
+    })
+}
+
+)
+.catch((err)=>{
+    console.log("mongoDB ocnnection failed",err)
+})
