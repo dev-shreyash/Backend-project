a3b4758a4504fbc20766cdf69793be6f6f12a474
Shreyash-69 <bhosaleshreyash2@gmail.com>
2024-04-07 20:30:08 +0530

    Implemented new user related routes for changing password, getting current user, updating account details, and managing user's avatar and cover image

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginuser, logoutuser, refreshAccesstoken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getUserWatchHistory, loginuser, logoutuser, refreshAccesstoken, registerUser, updateAccountDetails, updateUserAvater, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middelwares/multer.middelware.js";
 import { verifyJWT } from "../middelwares/auth.middleware.js";
 
@@ -26,5 +26,20 @@ router.route('/login').post(loginuser)
 router.route('/logout').post(verifyJWT, logoutuser)
 
 router.route("/refresh-token").post(refreshAccesstoken)
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+
+router.route("/current-user").post(verifyJWT ,getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single('image'), updateUserAvater)
+
+router.route("/cover-image").patch(verifyJWT,upload.single('/coverImage'), updateUserCoverImage)
+
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/history").get(verifyJWT, getUserWatchHistory)
+
 
 export default router
