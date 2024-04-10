6bf83374758bf7d5ff01c8d10bfff42b566a1222
Shreyash-69 <bhosaleshreyash2@gmail.com>
2024-04-10 13:01:56 +0530

    Added comments controller with getVideoComments and addComment functions; handle errors with ApiError and ApiResponse

---
+++ src/controllers/comments.controller.js
@@ -0,0 +1,49 @@
+import mongoose from "mongoose"
+import { Comment } from "../models/comment.model.js"
+import { ApiError } from "../utils/ApiError.js"
+import { ApiResponse } from "../utils/ApiResponse.js"
+import { asyncHandler } from "../utils/asyncHandler.js"
+
+
+
+const getVideoComments = asyncHandler(async(req, res)=>{
+    const videoId=req.params.videoId
+
+    const{page=1,limit=10}=req.query
+    try {
+
+        if(!videoId){
+            throw new ApiError(400,"video not found")
+        }
+
+
+        
+    } catch (error) {
+        console.error('Error getting comments:', error)
+        throw new ApiError(500, 'Server error, failed to get comments')     
+    }
+})
+
+
+const addComment =asyncHandler(async(req,res)=>
+{
+    const videoId=req.params.videoId
+    const {userId, content}=req.body
+
+   try {
+     if(!userId){
+         throw new ApiError(400,"userid not found")
+     }
+     if(!videoId){
+         throw new ApiError(400,"video not found")
+     }
+     
+   } catch (error) {
+    console.error('Error adding comments:', error)
+    throw new ApiError(500, 'Server error, failed to add comments')     
+   }
+})
+
+
+
+export {getVideoComments, addComment}
